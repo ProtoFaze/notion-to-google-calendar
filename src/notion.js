102c4bd2fb3bd01be725414b94c2a28cb13fb8c1
Damon <110209956+ProtoFaze@users.noreply.github.com>
2024-07-23 20:23:36 +0800

    Update notion.js

--- src/notion.js
+++ src/notion.js
@@ -1,40 +1,30 @@
-function getNotionData() {
-	const apiKey = PropertiesService.getScriptProperties().getProperty("NOTION_API_KEY");
-	const headers = {
-		Authorization: `Bearer ${apiKey}`,
-		"Notion-Version": "2022-06-28",
-	};
-
-	const data = {
-		filter: {
-			or: [
-				// {
-				// 	property: "Tags",
-				// 	multi_select: {
-				// 		contains: "Test2",
-				// 	},
-				// },
-				{
-					property: "Name",
-					title: {
-						is_not_empty: true,
-					},
-				},
-			],
-		},
-	};
-
-	const options = {
-		method: "POST",
-		contentType: "application/json",
-		headers: headers,
-		payload: JSON.stringify(data),
-	};
-
-	const response = UrlFetchApp.fetch(
-		"https://api.notion.com/v1/databases/10667571378c49ef9576f0bf33e53b9a/query",
-		options
-	);
-
-	return response.getContentText();
+function getNotionData(){
+  const apiKey = PropertiesService.getScriptProperties().getProperty("NOTION_API_KEY");
+  const headers = {
+    Authorization: `Bearer ${apiKey}`,
+    "Notion-Version": "2022-06-28",
+  };
+  const data = {
+    filter:{
+      or:[
+        {
+          property: "Name",
+          title:{
+            is_not_empty: true,
+          }
+        },
+      ],
+    },
+  }
+  const options = {
+    method: "POST",
+    contentType: "application/json",
+    headers: headers,
+    payload: JSON.stringify(data),
+  };
+  const response = UrlFetchApp.fetch(    
+    "https://api.notion.com/v1/databases/33dba351453745d9bed090ae869143a3/query",
+    options
+  );
+  return response.getContentText();
 }
